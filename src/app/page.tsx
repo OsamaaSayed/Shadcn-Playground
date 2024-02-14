42592ed0b22526be4ad894b2bd956ed24f32f748
Osama Sayed <osamasayedmuhammad@gmail.com>
2024-02-14 14:31:13 +0200

    ✨ add card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,58 @@
-import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-export default function Home() {
-  return <main>Home</main>;
+interface Recipe {
+  id: string;
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const res = await fetch("http://localhost:4000/recipes");
+
+  return res.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
+
+  return (
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card
+            key={recipe.id}
+            className="flex flex-col justify-between"
+          >
+            <CardHeader className="flex-row gap-4 items-center">
+              {/* //?{Avatar} */}
+
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+
+            <CardFooter className="flex justify-between">
+              <button>View Recipe</button>
+              {recipe.vegan && <p>Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
 }
